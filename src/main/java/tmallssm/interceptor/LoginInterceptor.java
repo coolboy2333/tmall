533bd8a24026eca3af883528e7ab12fba3693c7d
Danger <991151337@qq.com>
2018-12-19 20:56:41 +0800

    增加登录状态拦截器，自动识别无需和必须登录的页面

---
+++ src/main/java/tmallssm/interceptor/LoginInterceptor.java
@@ -0,0 +1,60 @@
+package tmallssm.interceptor;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import tmallssm.pojo.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter{
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handler) throws Exception{
+        HttpSession session = request.getSession();
+        String contextPath=session.getServletContext().getContextPath();
+        String[] noNeedAuthPage = new String[]{
+                "home",
+                "checkLogin",
+                "register",
+                "loginAjax",
+                "login",
+                "product",
+                "category",
+                "search"};
+
+        String uri = request.getRequestURI();
+        uri = StringUtils.remove(uri, contextPath);
+        if(uri.startsWith("/fore")){
+            String method = StringUtils.substringAfterLast(uri,"/fore" );
+            if(!Arrays.asList(noNeedAuthPage).contains(method)){
+                User user =(User) session.getAttribute("user");
+                if(null==user){
+                    response.sendRedirect("loginPage");
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    /**
+     * 在业务处理器处理请求执行完成后,生成视图之前执行的动作
+     * 可在modelAndView中加入数据，比如当前时间
+     */
+
+    public void postHandle(HttpServletRequest request,HttpServletResponse response, Object handler,ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 在DispatcherServlet完全处理完请求后被调用,可用于清理资源等
+     *
+     * 当有拦截器抛出异常时,会从当前拦截器往回执行所有的拦截器的afterCompletion()
+     */
+
+    public void afterCompletion(HttpServletRequest request,HttpServletResponse response, Object handler, Exception ex)throws Exception {
+
+    }
+}
